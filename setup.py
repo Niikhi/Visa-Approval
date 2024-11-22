d97dea4327a2ec2b35374b9fb16224ed15dc19de
Nikhil Ganesh <niga55522@gmail.com>
2024-11-22 23:53:50 +0530

    utils, logging,exception added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "us_visa",
+    version = "0.0.0",
+    author = "Nikhil",
+    author_email= "niga55522@gmail.com",
+    packages= find_packages()
+)
